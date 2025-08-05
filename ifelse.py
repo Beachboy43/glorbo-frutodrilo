6521e0c02981cf3c3649fc830aee6f93b5a4f973
Beachboy43 <A01799111@tec.mx>
2025-08-05 11:14:25 -0600

    Update ifelse.py

--- ifelse.py
+++ ifelse.py
@@ -1,8 +1,8 @@
 motion_detected = True   #Boolean from PIR sensor 
-light level = 20         #light level from ldr sensor (0-100, lower = darker)
+light_level = 20         #light level from ldr sensor (0-100, lower = darker)
 print("Light level", light_level, "%")
- print("Motion detected:", motion detected)
-if motion_detected and linght_level < 30:
+ print("Motion detected:", motion_detected)
+if motion_detected and light_level < 30:
   print("Light ON")
 else:
   print("Light OFF")
