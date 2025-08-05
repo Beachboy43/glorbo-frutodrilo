806c23fd9cda698a1bee7c86282dca95252767c4
Beachboy43 <A01799111@tec.mx>
2025-08-05 12:34:10 -0600

    Create battery_ monitoriing_system.py

---
+++ battery_ monitoriing_system.py
@@ -0,0 +1,8 @@
+battery_voltage = 3.5 
+print("Battery voltage:", battery_voltage, "V"
+if battery_voltage < 3.3:
+      print("Critical! Shut down system")
+elif battery_voltage < 3.7
+      print("Low battery! Please recharge")
+else:
+      print("Battery Ok")
