29bca24b71fcdbe314139ce5312f4b85197eed2e
Beachboy43 <A01799111@tec.mx>
2025-08-08 00:20:19 -0600

    Create randomloop.py

---
+++ randomloop.py
@@ -0,0 +1,37 @@
+import time
+import random
+
+def control_motor(is_on):
+  if is_on:
+    print("Motor On")
+  else:
+    print("Motor off")
+
+def check_temperature:
+  if temp > 40:
+    print("Critical: Shut down system")
+  elif temp > 30:
+    print("Warning: High temperature")
+  else:
+    print("Temp OK")
+
+def check_battery(Voltage):
+  if voltage < 3.3:
+    print("Critical: Shut down system")
+  elif voltage < 3.7:
+    print("Warning: Low battery")
+  else:
+    print("Battery OK")
+#----- continous monitoring____
+while True:
+  motor_state = random.choice([True, False])
+  temperature = randoom.uniform(20, 50)
+  battery_voltage = random.uniform(3.O, 4.2)
+
+  PRINT("N/---- SENSOR CHECK ----")
+  control_motor(motor_state)
+  check_temperature(temperature)
+  check_battery(battery_voltage)
+
+  time.seep(5)
+
